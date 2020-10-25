d63f31cd01957a546564ad3d99ee634a4bd4e48e
Rahimov Ruslan <dragon-2200@mail.ru>
2020-10-26 02:11:20 +0500

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const arr = Math.abs(n).toString().split("");
+    let newarr = [];
+    for (let i = arr.length - 1; 0 <= i; i--) {
+        newarr.push(arr[i]);
+    }
+
+    return Number(newarr.join(""));
+};
